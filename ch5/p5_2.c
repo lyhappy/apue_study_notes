3b1df9e4d5d1bf43cf623ccf2c167a28f31fc1ef
ly@laptop <luyao_happy@qq.com>
2017-05-26 20:55:57 +0800

    p5_2

---
+++ ch5/p5_2.c
@@ -0,0 +1,18 @@
+/*
+ * 程序清单5-1 Copy standard input to standard output using fgetc and fputc 
+ * 
+ */
+#include "apue.h"
+
+
+int main(void) {
+	char    buf[MAXLINE];
+
+	while (fgets(buf, MAXLINE, stdin) != NULL)
+	  if (fputs(buf, stdout) == EOF)
+		err_sys("output error");
+
+	if (ferror(stdin))
+	  err_sys("input error");
+	exit(0); 
+}
